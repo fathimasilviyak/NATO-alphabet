60a34e21a5a09ed65e8bfa6fceb9ed53e1dc857e
fathimasilviyak <silviyakvm@gmail.com>
2023-04-10 12:41:15 +0530

    exception handling for key error

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import pandas
+
 data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
 # print(data_frame)
 # create a dictionary of letter with corresponding word
-phonetic_dict = {row.letter:row.code for (index, row) in data_frame.iterrows()}
+phonetic_dict = {row.letter: row.code for (index, row) in data_frame.iterrows()}
+
+
 # print(phonetic_dict)
 # create a list of the phonetic code words from a word that the user inputs
-user_input = input("Enter a word: ").upper()
-new_list = [phonetic_dict[letter] for letter in user_input]
 
-print(new_list)
+def generate_phonetics():
+    user_input = input("Enter a word: ").upper()
+    try:
+        new_list = [phonetic_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetics()
+    else:
+        print(new_list)
+
+
+generate_phonetics()
